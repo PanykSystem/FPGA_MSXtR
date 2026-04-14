232281d2d4b5590bec6ffc6f1ef22bf0da7f4336
HRA! <hra@mvj.biglobe.ne.jp>
2026-04-15 06:32:18 +0900

    VDP側のテスト回路 青色LED点滅

---
+++ labo/FPGA_MSXtR_VDP_000/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2026 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12.02_SP2 (64-bit)
+//IP Version: 1.0
+//Part Number: GW2AR-LV18QN88C8/I7
+//Device: GW2AR-18
+//Created Time: Tue Apr 14 21:49:21 2026
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout), //output clkout
+        .clkoutd(clkoutd), //output clkoutd
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
