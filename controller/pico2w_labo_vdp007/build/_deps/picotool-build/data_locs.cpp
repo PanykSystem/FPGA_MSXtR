da8571d8ecc2da5299cef392b7387ff8eac69595
HRA! <hra@mvj.biglobe.ne.jp>
2026-05-30 09:20:06 +0900

    Updated

--- controller/pico2w_labo_vdp007/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/mnt/c/Users/hra/Documents/github/HRA_product/FPGA_MSXtR/controller/pico2w_labo_vdp007/build/_deps/picotool/"
-};
